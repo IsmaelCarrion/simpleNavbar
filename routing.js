db1580193aa9ed363d0a11454f352e08a26cb6c0
Ismael Carrion <iecarrion@enalsa.net>
2023-10-30 19:03:15 -0600

    Add files via upload

---
+++ routing.js
@@ -0,0 +1,26 @@
+// This is a function that takes a page path as an argument
+// You need to make the function load the content of that page into the "content" div on the index file
+window.addEventListener('hashchange', loadPage);
+
+
+async function loadPage() {
+
+    let page = location.hash.replace('#', '');
+
+    // fetch the page, use await
+    const res = await fetch(page);
+    
+    // get text from res, use await
+    const content = await res.text();
+
+    // get the element with id 'content'
+    const element = document.getElementById('content');
+
+    // set innerHTML of the element
+    // your code goes here
+    element.innerHTML = content;
+    
+  }
+  
+
+
